2995bb60e5fb8f8f9677a8fd10c7ddc0d022e7db
amitmalhotra <amit030319@gmail.com>
2023-11-05 00:20:30 +0530

    base_model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.CnnClassifier import logger
 from CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -7,6 +9,18 @@ STAGE_NAME = "Data ingestion stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<")
+        
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "PREPARE BASE MODEL"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<")
         
